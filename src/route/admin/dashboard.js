3ec019f53253aab32b0ad104a052346435eb864a
Ikechukwu Onyekanna <ionyekanna@gmail.com>
2020-10-12 14:14:47 +0100

    Update

--- src/route/admin/dashboard.js
+++ src/route/admin/dashboard.js
@@ -135,45 +135,6 @@ class App extends Component {
                     </div>
                 </div>
 
-                <div className="wpo-mission-area section-padding">
-                    <div className="container">
-                        <h3 style={{ marginBottom: '40px', textAlign: 'center' }} >Campaigns</h3>
-                        <div className="wpo-mission-wrap">
-                            <div className="row">
-                                {
-                                    result && result.list && result.list.length > 0 && result.list.map((item, i) => {
-                                        return <div className="col-lg-4 col-md-6 col-sm-12 col-12 custom-grid" key={i} style={{ marginTop: '45px' }} >
-                                            <div className="wpo-event-item">
-                                                <div className="wpo-event-img">
-                                                    <img src={UrlService.FILE_BASE_PATH + item.coverPhoto} alt="" />
-                                                    {/* <div className="thumb-text"><span>25</span><span>NOV</span></div> */}
-                                                </div>
-                                                <div className="wpo-event-text slide-caption">
-                                                    <h2>{item.title}</h2>
-                                                    <ul>
-                                                        <li><i className="fa fa-clock-ow" aria-hidden="true" />Goal</li>
-                                                        <li><i className="fi flaticon-pinsw" />N{item.goal.toFixed(2).replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')}</li>
-
-                                                    </ul>
-                                                    <p>{item.story}</p>
-                                                    {!item.enabled && <button className="MuiButtonBase-root MuiButton-root MuiButton-text cBtn cBtnSmall cBtnThemer MuiButton-halfWidth" style={{ float: 'right', 'backgroundColor': 'green', color: 'white' }} onClick={() => this.activateCampaign(item, true)} tabIndex={0} ><span className="MuiButton-label">Activate</span><span className="MuiTouchRipple-root" /></button>}
-                                                    {item.enabled && <button className="MuiButtonBase-root MuiButton-root MuiButton-text cBtn cBtnSmall cBtnThemer MuiButton-halfWidth" style={{ float: 'right', 'backgroundColor': 'green', color: 'white' }} onClick={() => this.activateCampaign(item, false)} tabIndex={0} ><span className="MuiButton-label">Deactivate</span><span className="MuiTouchRipple-root" /></button>}
-                                                    <br />
-                                                    <Link to={"/campaign/detail/"+item.id}>Learn More...</Link>
-                                                    <div className="btns" >
-                                                        <Link className="theme-btn" to="/donate" style={{ color: 'white' }} >Donate Now</Link>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    })
-                                }
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-
                 <div className="wpo-counter-area undefined">
                     <div className="container">
                         <div className="row">
@@ -198,6 +159,47 @@ class App extends Component {
                         </div>
                     </div>
                 </div>
+
+                <div className="wpo-mission-area section-padding">
+                    <div className="container">
+                        <h3 style={{ marginBottom: '40px', textAlign: 'center' }} >Campaigns</h3>
+                        <div className="wpo-mission-wrap">
+                            <div className="row">
+                                {
+                                    result && result.list && result.list.length > 0 && result.list.map((item, i) => {
+                                        return <div className="col-lg-4 col-md-6 col-sm-12 col-12 custom-grid" key={i} style={{ marginTop: '45px' }} >
+                                            <div className="wpo-event-item">
+                                                <div className="wpo-event-img">
+                                                    <img src={UrlService.FILE_BASE_PATH + item.coverPhoto} alt="" />
+                                                    {/* <div className="thumb-text"><span>25</span><span>NOV</span></div> */}
+                                                </div>
+                                                <div className="wpo-event-text slide-caption">
+                                                    <h2>{item.title}</h2>
+                                                    <ul>
+                                                        <li><i className="fa fa-clock-ow" aria-hidden="true" />Goal</li>
+                                                        <li><i className="fi flaticon-pinsw" />N{item.goal.toFixed(2).replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')}</li>
+
+                                                    </ul>
+                                                    <p>{item.story}</p>
+                                                    {!item.enabled && <button className="MuiButtonBase-root MuiButton-root MuiButton-text cBtn cBtnSmall cBtnThemer MuiButton-halfWidth" style={{ float: 'right', 'backgroundColor': 'green', color: 'white' }} onClick={() => this.activateCampaign(item, true)} tabIndex={0} ><span className="MuiButton-label">Activate</span><span className="MuiTouchRipple-root" /></button>}
+                                                    {item.enabled && <button className="MuiButtonBase-root MuiButton-root MuiButton-text cBtn cBtnSmall cBtnThemer MuiButton-halfWidth" style={{ float: 'right', 'backgroundColor': 'red', color: 'white' }} onClick={() => this.activateCampaign(item, false)} tabIndex={0} ><span className="MuiButton-label">Deactivate</span><span className="MuiTouchRipple-root" /></button>}
+                                                    <br />
+                                                    <Link to={"/campaign/detail/"+item.id}>Learn More...</Link>
+                                                    <div className="btns" >
+                                                        <Link className="theme-btn" to="/donate" style={{ color: 'white' }} >Donate Now</Link>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    })
+                                }
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+                
 
                 <Footer />>
             <div className="col-lg-12">
